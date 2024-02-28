bec39b2f8dc5836eb3f846a42aa9a11c87726976
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2024-02-28 18:22:55 +0100

    setup server

---
+++ data/Products.js
@@ -0,0 +1,42 @@
+export const products = [
+    {
+        _id : "1",
+        name : "product 1",
+        image : "",
+        desc : "Lorem Ipsum is dash dash dash and so on",
+        price : "89",
+        countInStock : "4",
+        rating : "4",
+        numReviews : "12"
+    },
+    {
+        _id : "4",
+        name : "product 4",
+        image : "",
+        desc : "Lorem Ipsum is dash dash dash and so on",
+        price : "92",
+        countInStock : "14",
+        rating : "4",
+        numReviews : "12"
+    },
+    {
+        _id : "3",
+        name : "product 3",
+        image : "",
+        desc : "Lorem Ipsum is dash dash dash and so on",
+        price : "18",
+        countInStock : "12",
+        rating : "2",
+        numReviews : "2"
+    },
+    {
+        _id : "4",
+        name : "product 4",
+        image : "",
+        desc : "Lorem Ipsum is dash dash dash and so on",
+        price : "16",
+        countInStock : "32",
+        rating : "5",
+        numReviews : "25"
+    },
+]
---
+++ server.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+import express from 'express';
+//import allRoutes from './routes/allRoutes.js';
+import cors from 'cors';
+import { products } from './data/Products.js';
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Database Configuration
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/api/products', (req, res) => {
+    res.json(products)
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
