128d74eb5f97692c5fd7e8b6281b8f6e15237d69
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2024-03-05 22:26:01 +0100

    implemented sign up endpoint

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -3,7 +3,7 @@ import User from '../Models/user.js';
 import generateTOken from '../utils/generateToken.js';
 
 
-
+//LOGIN
 export const login_user = asyncHandler(async (req, res) => {
     const { email, password } = req.body
     const user = await User.findOne({ email })
@@ -23,6 +23,34 @@ export const login_user = asyncHandler(async (req, res) => {
     }
 })
 
+//REGISTER
+export const register_user = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error("User already exists")
+    }
+    const user = await User.create({
+        name, email, password
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateTOken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error("Invalid User Data")
+    }
+})
+
+//GET PROFILE
 export const get_user_profile = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id)
 
@@ -34,7 +62,33 @@ export const get_user_profile = asyncHandler(async (req, res) => {
             isAdmin: user.isAdmin,
             createdAt: user.createdAt,
         })
-    }else{
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+//UPDATE PROFILE
+export const update_user_profile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+
+        if(req.body.password){
+            user.password = req.body.password
+        }
+        const updateUser = await user.save()
+        res.json({
+            _id: updateUser._id,
+            name: updateUser.name,
+            email: updateUser.email,
+            isAdmin: updateUser.isAdmin,
+            createdAt: updateUser.createdAt,
+            token: generateTOken(updateUser._id),
+        })
+    } else {
         res.status(404)
         throw new Error('User not found')
     }
--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import protect from '../Middleware/authMiddleware.js';
-import { get_user_profile, login_user } from '../Controllers/userController.js';
+import { get_user_profile, login_user, register_user, update_user_profile } from '../Controllers/userController.js';
 
 const userRoute = express.Router()
 
 //LOGIN
 userRoute.post("/login", login_user)
+//REGISTER
+userRoute.post("/register", register_user)
 //GET USER PROFILE
 userRoute.get("/profile", protect, get_user_profile)
+//UPDATE USER PROFILE
+userRoute.put("/profile", protect, update_user_profile)
 
 
 
